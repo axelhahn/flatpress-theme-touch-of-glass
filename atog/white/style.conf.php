31095e00b6f8d5e3732c9edc27dd303996e96269
Axel Hahn <ax2002@gmx.net>
2022-06-26 13:33:05 +0200

    add a white theme

---
+++ atog/white/style.conf.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * ___________________________________________________________________________
+ * 
+ *        Axels theme
+ *        _______      _______                    __       
+ *       |   _   |    |_     _|.-----.--.--.----.|  |--.   
+ *       |       |      |   |  |  _  |  |  |  __||     |   
+ *       |___|___|      |___|  |_____|_____|____||__|__|   
+ *        _______   ___      _______ __                    
+ *       |       |.'  _|    |     __|  |.---.-.-----.-----.
+ *       |   -   ||   _|    |    |  |  ||  _  |__ --|__ --|
+ *       |_______||__|      |_______|__||___._|_____|_____|
+ * ___________________________________________________________________________
+ * 
+ * T E A L
+ * ___________________________________________________________________________
+ * 
+ * Style Name: White
+ * Style URI: https://github.com/axelhahn/flatpress-theme-touch-of-glass
+ * Description: Axels theme Touch Of Glass simple and lightweight 
+ * Version: 1.000
+ * Author: Axel Hahn
+ * Author URI: https://www.axel-hahn.de/
+ */
+$style ['name'] = 'White';
+$style ['author'] = 'Axel Hahn';
+$style ['www'] = 'https://www.axel-hahn.de/';
+
+$style ['version'] = 1.000;
+
+$style ['style_def'] = 'style.css';
+$style ['style_admin'] = 'admin.css';
+$style ['style_print'] = 'print.css';
+$style ['style'] = 'default';
